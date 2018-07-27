cdeb343aac2fba82a986e3acafa2d43f2e77567c
Zachary Baklund <zjbaklund@rfsdelivers.com>
2018-07-27 13:46:48 -0500

    fixed bug in same component routing w/ modal

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { EventService } from '../shared/event.service';
 import { IEvent, ISession } from '../shared/index';
 
@@ -21,9 +21,10 @@ export class EventDetailsComponent implements OnInit {
                 private route: ActivatedRoute) { }
     
     ngOnInit() {
-        this.event = this.eventService.getEvent
-          (+this.route.snapshot.params['id']);
-
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+            this.resetState();
+        });
     }
 
     addSession() {
@@ -40,5 +41,11 @@ export class EventDetailsComponent implements OnInit {
         this.event.sessions.push(session);
         this.eventService.updateEvent(this.event);
         this.addMode = false;
+    }
+
+    resetState() {
+        this.addMode = false;
+        this.filterBy = 'all';
+        this.sortBy = 'votes';
     }
 }
