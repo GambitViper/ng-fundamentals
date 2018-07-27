8b6e34b02549881bf1bd0a4e7a316723ae1e6000
Zachary Baklund <zjbaklund@rfsdelivers.com>
2018-07-27 09:12:21 -0500

    sorting and filtering sessions in the event-detail

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -6,13 +6,15 @@ import { Component, Input } from '@angular/core';
     <div (click)="toggleContent()" class="well pointable">
       <h4>
         <ng-content select="[data-well-title]"></ng-content>
+        <ng-content select="[data-chevron-down]" *ngIf="visible == false"></ng-content>
+        <ng-content select="[data-chevron-up]" *ngIf="visible"></ng-content>
       </h4>
       <ng-content select="[data-well-body]" *ngIf="visible"></ng-content>
     </div>
     `
 })
 export class CollapsibleWellComponent {
-    visible: boolean = false;
+    visible: boolean = true;
 
     toggleContent() {
         this.visible = !this.visible;
